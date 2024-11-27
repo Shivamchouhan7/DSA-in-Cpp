4b579e207a2d133483c560492904e1b651854093
shivamchouhan7 <shivamchouhan123sc@gmail.com>
2024-11-27 09:05:38 +0530

    Bitwise  left shift operator <<

--- Miscellaneous Concepts/Bitwise operator/Bitwise_left_shift.cpp
+++ Miscellaneous Concepts/Bitwise operator/Bitwise_left_shift.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 // Bitwise << (left shift) operator "<<"
 /* Let say for  any number n
-if we write n << i
+if we write n << i (it can also be seen as [n multiplied by 2 to the power i])
 means n shifted towards left i times 
 For Example :-
 let say 4 << 1
