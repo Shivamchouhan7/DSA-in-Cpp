ccbf80a638915b3530d2c8130bc6d360edc0b45c
shivamchouhan7 <shivamchouhan123sc@gmail.com>
2024-11-27 08:59:21 +0530

    Bitwise Right shift operator >>

---
+++ Miscellaneous Concepts/Bitwise operator/Bitwise_Right_shift.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+// Bitwise Right shift operator ">>"
+/* Let say for  any number n
+if we write n >> i
+means n shifted towards Right i times 
+For Example :-
+let say 4 >> 1
+means [100] and after shifting it becomes [10] means 2
+extra bit comes outside as we don't have any space at right side   */
+int main()
+{
+    int a, i;
+    cout << "Enter  number: ";
+    cin >> a;
+    cout << "Enter number of shifts : ";
+    cin >> i;
+
+    int result = a>>i; // using bitwise >> (Right shift ) Operator 
+
+    cout << i << " times  Right shift of " << a  << " is " << result << endl;
+    cout<<"It can be seen as "<<a<< " >> "<< i;
+
+    return 0;
+}
