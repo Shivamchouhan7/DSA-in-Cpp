c2ee86f38dbaa50ad12c7cadea1a4b682e5fd7aa
shivamchouhan7 <shivamchouhan123sc@gmail.com>
2024-11-27 08:48:10 +0530

    Bitwise << (left shift) operator

---
+++ Miscellaneous Concepts/Bitwise operator/Bitwise_left_shift.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// Bitwise << (left shift) operator "<<"
+/* Let say for  any number n
+if we write n << i
+means n shifted towards left i times 
+For Example :-
+let say 4 << 1
+means 100 and after shifting it becomes 1000 means 8  */
+int main()
+{
+    int a, i;
+    cout << "Enter  number: ";
+    cin >> a;
+    cout << "Enter number of shifts : ";
+    cin >> i;
+
+    int result = a<<i; // using bitwise << (left shift )Operator 
+
+    cout << i << " times  left shift of " << a  << " is " << result << endl;
+    cout<<"It can be seen as "<<a<< " << "<< i;
+
+    return 0;
+}
