df00efbbbed7e9f5bb3a37f58a0f5fa478a6ca8d
shivamchouhan7 <shivamchouhan123sc@gmail.com>
2025-04-28 22:21:42 +0530

    return pair in sorted vector with target sum

---
+++ Vector/pair_sum01.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// return pair in sorted array / vector with target sum
+// Brute approach
+
+vector<int> pairsum(vector<int> nums, int target){
+    vector<int> ans;
+    int size= nums.size();
+    for(int i=0;i<size;i++){
+        for(int j=i+1;j<size;j++){
+            if(nums[i]+nums[j]==target){
+                ans.push_back(nums[i]);
+                ans.push_back(nums[j]);
+                return ans;
+            }
+            
+        }
+    }
+    return ans;
+
+}
+int main(){
+    int n,target;
+    cout<<"Enter a size of array / vector :- ";
+    cin>>n;
+    vector<int> vec(n) ;
+    cout<<"Enter the elements :- \n ";
+    for(int &ele:vec){
+        cin>>ele;
+    }
+    cout<<"Enter target value :";
+    cin>>target;
+    vector<int> ans=pairsum(vec,target);
+    if(!ans.empty()){
+        cout<<"Pair for target value " <<target<<" is :- "<< endl;
+        cout<<"("<<ans[0]<<","<<ans[1]<<")";
+    }
+    else{
+        cout << "No pair found for target value " << target << "." << endl;
+    }
+
+    return 0;
+}
